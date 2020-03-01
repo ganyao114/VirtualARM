39aa8503b97716d15b32db2efa10f099d6103244
swift_gan <939543405@qq.com>
2020-03-01 19:54:54 +0800

    rewrite dbi context

--- app/src/main/native/dbi/arm64/dbi_context_arm64.cc
+++ app/src/main/native/dbi/arm64/dbi_context_arm64.cc
@@ -7,70 +7,14 @@
 
 using namespace DBI::A64;
 
-Context::Context() : masm_{} {
-    HOST_TLS[CTX_TLS_SLOT] = &context_;
+Context::Context(const Register &reg_ctx) : masm_{}, REG_CTX{reg_ctx} {
     suspend_addr_ = static_cast<u64 *>(mmap(0, PAGE_SIZE, PROT_READ,
                                             MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED,
                                             -1, 0));
-    HOST_TLS[SUSP_TLS_SLOT] = suspend_addr_;
 }
 
 Context::~Context() {
     munmap(suspend_addr_, PAGE_SIZE);
-}
-
-void Context::WrapContext(std::function<void()> wrap) {
-    __ Push(REG_CTX);
-    __ Mrs(REG_CTX, TPIDR_EL0);
-    __ Ldr(REG_CTX, MemOperand(REG_CTX, CTX_TLS_SLOT * 8));
-    // save tmp0, tmp1
-    __ Str(TMP0, MemOperand(REG_CTX, OFFSET_CTX_A64_CPU_REG + 17 * 8));
-    __ Pop(TMP0);
-    __ Str(TMP0, MemOperand(REG_CTX, OFFSET_CTX_A64_CPU_REG + 16 * 8));
-    wrap();
-    // restore tmp0, tmp1
-    __ Ldp(TMP1, TMP0, MemOperand(REG_CTX, OFFSET_CTX_A64_CPU_REG + 16 * 8));
-}
-
-void Context::GetSp(u8 target) {
-    auto rt = XRegister::GetXRegFromCode(target);
-    __ Ldr(rt, MemOperand(REG_CTX, OFFSET_CTX_A64_SP));
-}
-
-void Context::GetPc(u8 target) {
-    auto rt = XRegister::GetXRegFromCode(target);
-    __ Ldr(rt, MemOperand(REG_CTX, OFFSET_CTX_A64_PC));
-}
-
-void Context::SavePc(VAddr pc) {
-    __ Mov(TMP0, pc);
-    __ Str(TMP0, MemOperand(REG_CTX, OFFSET_CTX_A64_PC));
-}
-
-void Context::SaveSp() {
-    __ Str(sp, MemOperand(REG_CTX, OFFSET_CTX_A64_SP));
-}
-
-void Context::LookupPageTable(u8 reg_addr) {
-    // TODO pick tmp regs
-    __ Mov(TMP1, page_tabel_addr_);
-}
-
-void Context::LookupPageTable(VAddr const_addr, u8 reg) {
-
-}
-
-void Context::SetSuspendFlag(bool suspend) {
-    mprotect(suspend_addr_, PAGE_SIZE, suspend ? PROT_NONE : PROT_READ);
-}
-
-void Context::CheckSuspend(bool in_context_wrap) {
-    if (in_context_wrap) {
-        __ Mrs(TMP0, TPIDR_EL0);
-        __ Ldr(TMP0, MemOperand(TMP0, SUSP_TLS_SLOT * 8));
-        // if suspend, trigger 11 signal
-        __ Ldr(TMP0, MemOperand(TMP0));
-    }
 }
 
 const CPU::A64::CPUContext &Context::GetContext() const {
@@ -85,7 +29,53 @@ void Context::SetCurPc(VAddr cur_pc) {
     cur_pc_ = cur_pc;
 }
 
-void Context::ReadTpidr(u8 target) {
+void Context::SetSuspendFlag(bool suspend) {
+    mprotect(suspend_addr_, PAGE_SIZE, suspend ? PROT_NONE : PROT_READ);
+}
+
+ContextNoMemTrace::ContextNoMemTrace() : Context(TMP1) {
+    HOST_TLS[CTX_TLS_SLOT] = &context_;
+    HOST_TLS[SUSP_TLS_SLOT] = suspend_addr_;
+}
+
+void ContextNoMemTrace::WrapContext(std::function<void()> wrap) {
+    __ Push(REG_CTX);
+    __ Mrs(REG_CTX, TPIDR_EL0);
+    __ Ldr(REG_CTX, MemOperand(REG_CTX, CTX_TLS_SLOT * 8));
+    // save tmp0, tmp1
+    __ Str(TMP0, MemOperand(REG_CTX, OFFSET_CTX_A64_CPU_REG + 17 * 8));
+    __ Pop(TMP0);
+    __ Str(TMP0, MemOperand(REG_CTX, OFFSET_CTX_A64_CPU_REG + 16 * 8));
+    wrap();
+    // restore tmp0, tmp1
+    __ Ldp(TMP1, TMP0, MemOperand(REG_CTX, OFFSET_CTX_A64_CPU_REG + 16 * 8));
+}
+
+void ContextNoMemTrace::GetSp(u8 target) {
+    auto rt = XRegister::GetXRegFromCode(target);
+    __ Ldr(rt, MemOperand(REG_CTX, OFFSET_CTX_A64_SP));
+}
+
+void ContextNoMemTrace::GetPc(u8 target) {
+    auto rt = XRegister::GetXRegFromCode(target);
+    __ Ldr(rt, MemOperand(REG_CTX, OFFSET_CTX_A64_PC));
+}
+
+void ContextNoMemTrace::SavePc(VAddr pc) {
+    __ Mov(TMP0, pc);
+    __ Str(TMP0, MemOperand(REG_CTX, OFFSET_CTX_A64_PC));
+}
+
+void ContextNoMemTrace::CheckSuspend(bool in_context_wrap) {
+    if (in_context_wrap) {
+        __ Mrs(TMP0, TPIDR_EL0);
+        __ Ldr(TMP0, MemOperand(TMP0, SUSP_TLS_SLOT * 8));
+        // if suspend, trigger 11 signal
+        __ Ldr(TMP0, MemOperand(TMP0));
+    }
+}
+
+void ContextNoMemTrace::ReadTPIDR(u8 target) {
     auto wrap = [this, target]() -> void {
         if (TMP0.GetCode() == target || TMP1.GetCode() == target) {
             __ Ldr(TMP0, MemOperand(REG_CTX, OFFSET_CTX_A64_TPIDR));
@@ -97,7 +87,7 @@ void Context::ReadTpidr(u8 target) {
     WrapContext(wrap);
 }
 
-void Context::ReadTpidrro(u8 target) {
+void ContextNoMemTrace::ReadTPIDRRO(u8 target) {
     auto wrap = [this, target]() -> void {
         if (TMP0.GetCode() == target || TMP1.GetCode() == target) {
             __ Ldr(TMP0, MemOperand(REG_CTX, OFFSET_CTX_A64_TPIDRRO));
@@ -110,7 +100,7 @@ void Context::ReadTpidrro(u8 target) {
 }
 
 
-void ContextSwitch::SaveContextFull() {
+void ContextNoMemTrace::SaveContextFull() {
     auto wrap = [this]() -> void {
         // XRegs
         for (int i = 0; i < 32; ++i) {
@@ -140,7 +130,7 @@ void ContextSwitch::SaveContextFull() {
     WrapContext(wrap);
 }
 
-void ContextSwitch::RestoreContextFull() {
+void ContextNoMemTrace::RestoreContextFull() {
     auto wrap = [this]() -> void {
         // XRegs
         for (int i = 0; i < 32; ++i) {
@@ -165,4 +155,71 @@ void ContextSwitch::RestoreContextFull() {
         }
     };
     WrapContext(wrap);
+}
+
+ContextWithMemTrace::ContextWithMemTrace() : Context(TMP1) {
+    // Need keep CTX_REG, so rewrite all instructions used CTX_REG
+}
+
+void ContextWithMemTrace::LookupPageTable(u8 reg_addr) {
+    constexpr static int PAGE_BITS = 12;
+    auto rt = XRegister::GetXRegFromCode(reg_addr);
+    auto [tmp_reg1, tmp_reg2] = PeekTmpRegs(reg_addr);
+    auto tmp1 = XRegister::GetXRegFromCode(tmp_reg1);
+    auto tmp2 = XRegister::GetXRegFromCode(tmp_reg2);
+    PushX(tmp_reg1, tmp_reg2);
+    __ Mov(tmp1, Operand(XRegister::GetXRegFromCode(reg_addr), LSR, PAGE_BITS));
+    __ Mov(tmp2, page_tabel_addr_);
+    __ Add(tmp1, tmp2, Operand(tmp1, LSL, 3));
+    __ Ldr(rt, MemOperand(tmp1));
+    PopX(tmp_reg1, tmp_reg2);
+}
+
+void ContextWithMemTrace::LookupPageTable(VAddr const_addr, u8 reg) {
+    constexpr static int PAGE_BITS = 12;
+    auto rt = XRegister::GetXRegFromCode(reg);
+    __ Mov(rt ,page_tabel_addr_ + (const_addr << PAGE_BITS) * 8);
+    __ Ldr(rt, MemOperand(rt));
+}
+
+void ContextWithMemTrace::PushX(u8 reg1, u8 reg2) {
+    assert(reg1 != REG_CTX.GetCode() && reg2 != REG_CTX.GetCode());
+    if (reg2 == NO_REG) {
+        auto rt = XRegister::GetXRegFromCode(reg1);
+        __ Str(rt, MemOperand(REG_CTX, OFFSET_CTX_A64_CPU_REG + rt.GetCode() * 8));
+    } else {
+        auto rt1 = XRegister::GetXRegFromCode(reg1);
+        auto rt2 = XRegister::GetXRegFromCode(reg2);
+        if (reg2 - reg1 == 1) {
+            __ Stp(rt1, rt2, MemOperand(REG_CTX, OFFSET_CTX_A64_CPU_REG + rt1.GetCode() * 8));
+        } else {
+            __ Str(rt1, MemOperand(REG_CTX, OFFSET_CTX_A64_CPU_REG + rt1.GetCode() * 8));
+            __ Str(rt2, MemOperand(REG_CTX, OFFSET_CTX_A64_CPU_REG + rt2.GetCode() * 8));
+        }
+    }
+}
+
+void ContextWithMemTrace::PopX(u8 reg1, u8 reg2) {
+    assert(reg1 != REG_CTX.GetCode() && reg2 != REG_CTX.GetCode());
+    if (reg2 == NO_REG) {
+        auto rt = XRegister::GetXRegFromCode(reg1);
+        __ Ldr(rt, MemOperand(REG_CTX, OFFSET_CTX_A64_CPU_REG + rt.GetCode() * 8));
+    } else {
+        auto rt1 = XRegister::GetXRegFromCode(reg1);
+        auto rt2 = XRegister::GetXRegFromCode(reg2);
+        if (reg2 - reg1 == 1) {
+            __ Ldp(rt1, rt2, MemOperand(REG_CTX, OFFSET_CTX_A64_CPU_REG + rt1.GetCode() * 8));
+        } else {
+            __ Ldr(rt1, MemOperand(REG_CTX, OFFSET_CTX_A64_CPU_REG + rt1.GetCode() * 8));
+            __ Ldr(rt2, MemOperand(REG_CTX, OFFSET_CTX_A64_CPU_REG + rt2.GetCode() * 8));
+        }
+    }
+}
+
+std::pair<u8, u8> ContextWithMemTrace::PeekTmpRegs(u8 reg_target) {
+    u8 r1 = 0;
+    for (int i = 0; i < 30; ++i) {
+
+    }
+    return std::make_pair(0, 1);
 }
--- app/src/main/native/dbi/arm64/dbi_context_arm64.h
+++ app/src/main/native/dbi/arm64/dbi_context_arm64.h
@@ -13,55 +13,84 @@ using namespace vixl::aarch64;
 namespace DBI::A64 {
 
 #define __ masm_.
+#define NO_REG 255
 #define CTX_TLS_SLOT 7
 #define SUSP_TLS_SLOT 8
 #define TMP0 x17
 #define TMP1 x16
-#define REG_CTX TMP1
 #define HOST_TLS ({ void** __val; __asm__("mrs %0, tpidr_el0" : "=r"(__val)); __val; })
 
     class Context {
     public:
 
-        Context();
-
+        Context(const Register &reg_ctx);
         virtual ~Context();
-
-        void SetSuspendFlag(bool suspend);
 
         const CPU::A64::CPUContext &GetContext() const;
 
         VAddr GetCurPc() const;
         void SetCurPc(VAddr cur_pc);
 
-        CPU::A64::CPUContext context_;
-        VAddr cur_pc_;
-        VAddr page_tabel_addr_{0};
+        void SetSuspendFlag(bool suspend);
 
-    protected:
+        virtual void GetSp(u8 target) {};
+        virtual void GetPc(u8 target) {};
 
-        void WrapContext(std::function<void()> wrap);
-        void SaveSp();
-        void SavePc(VAddr pc);
-        void GetSp(u8 target);
-        void GetPc(u8 target);
-
-        void LookupPageTable(u8 addr_reg);
-        void LookupPageTable(VAddr const_addr, u8 reg);
-
-        void CheckSuspend(bool in_context_wrap);
+        virtual void CheckSuspend(bool in_context_wrap) {};
+        virtual void SavePc(VAddr pc) {};
 
         //sysreg
-        void ReadTpidr(u8 target);
-        void ReadTpidrro(u8 target);
+        virtual void ReadTPIDR(u8 target) {};
+        virtual void ReadTPIDRRO(u8 target) {};
 
+        // context switch
+        virtual void SaveContextFull() {};
+        virtual void RestoreContextFull() {};
+
+        CPU::A64::CPUContext context_;
+        VAddr cur_pc_;
+
+    protected:
+        const Register &REG_CTX;
         MacroAssembler masm_;
         u64 *suspend_addr_;
     };
 
-    class ContextSwitch : public Context {
-        void SaveContextFull();
-        void RestoreContextFull();
+    class ContextNoMemTrace : public Context {
+    protected:
+        void WrapContext(std::function<void()> wrap);
+
+    public:
+        ContextNoMemTrace();
+
+        void GetSp(u8 target) override;
+        void GetPc(u8 target) override;
+
+        void CheckSuspend(bool in_context_wrap) override;
+
+        void SavePc(VAddr pc) override;
+
+        void ReadTPIDR(u8 target) override;
+        void ReadTPIDRRO(u8 target) override;
+
+        void SaveContextFull() override;
+        void RestoreContextFull() override;
+    };
+
+    class ContextWithMemTrace : public Context {
+    public:
+        ContextWithMemTrace();
+
+        void LookupPageTable(u8 addr_reg);
+        void LookupPageTable(VAddr const_addr, u8 reg);
+
+        VAddr page_tabel_addr_{0};
+
+    protected:
+        void PushX(u8 reg1, u8 reg2 = NO_REG);
+        void PopX(u8 reg1, u8 reg2 = NO_REG);
+
+        std::pair<u8, u8> PeekTmpRegs(u8 reg_target);
     };
 
 }
